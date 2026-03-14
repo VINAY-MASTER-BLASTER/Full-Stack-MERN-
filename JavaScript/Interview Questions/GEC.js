fb439a5f51d13ea8c74ecc08c7cbfdd44335ce7b
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-03-14 15:12:26 +0530

    Global Execution Context

---
+++ JavaScript/Interview Questions/GEC.js
@@ -0,0 +1,19 @@
+// ? Global Execution Context
+
+// x = 30
+// console.log("Value of x is ",x)
+// var x = 10
+
+
+// ? Global Execution Context
+console.log("Glbal Execution Context Start")
+var globalvariable = "i am a global variable"
+function globalFunction(){
+    console.log("Inside global Functions")
+}
+console.log(globalvariable)
+globalFunction()
+console.log("Global Execution Context Ends")
+
+
+
