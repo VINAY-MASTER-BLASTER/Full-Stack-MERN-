e962a1e08d8da7f6fb25a553ec503a513c5b3f2a
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2026-03-15 11:05:19 +0530

    Hosting

---
+++ JavaScript/Interview Questions/Hoisting.js
@@ -0,0 +1,18 @@
+// javaScript Hoisting 
+
+console.log(a)
+// getname()
+
+var a = 7
+
+// function getname(){
+//     console.log("Vinay")
+// }
+// var getname = () =>{
+//     console.log("Hello Vinay")
+// }
+//  const getname = function(){
+//     console.log("Hello World ")
+//  }
+
+
